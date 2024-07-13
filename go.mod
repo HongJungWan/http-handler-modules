9d4f30e3e0b8242afe974b19f855abd80cba51f0
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-07-13 19:55:55 +0900

    feat: http server (#1)
    1. project init
    2. web server action
    3. refactoring and test code
    4. dynamically change the port number

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Hong/http-handler-modules
+
+go 1.20
+
+require golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
+}
+
+// Errgroup with context 패턴
+func run(ctx context.Context, l net.Listener) error {
+	s := &http.Server{
+		// 인수로 받은 net.Listener를 사용하므로, Addr 필드는 지정하지 않음
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		// ListenAndServe 메서드가 아닌 Serve 메서드로 변경
+		if err := s.Serve(l); err != nil &&
+			// http.ErrServerClosed는 http.Server.Shutdown()이 정상적으로 종료되었음을 나타내므로 이상이 아님
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// 채널로부터의 알림(종료 알림)을 기다림
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	// Go 메서드로 실행한 다른 고루틴의 종료를 기다림.
+	return eg.Wait()
+}
