ef3eca5e5adfe20cf2afee2b739dccd8bf45fbcc
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2024-07-17 10:02:16 +0900

    refactor: refactor the HTTP server to a loosely coupled architecture.
    1. Implemented functionality to read configuration information from environment variables.
    2. Implemented normal termination upon receiving a termination signal.
    3. Organized the execution order of the HTTP server in the Server type.
    4. Separated route definitions into the NewMux function.
    5. Wrote test code for HTTP handlers using the httptest package.

--- main.go
+++ main.go
@@ -6,12 +6,7 @@ import (
 	"github.com/Hong/http-handler-modules/config"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -21,47 +16,18 @@ func main() {
 	}
 }
 
-// Errgroup with context 패턴
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	cfg, err := config.NewConfig()
 	if err != nil {
 		return err
 	}
-
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
-
-	s := &http.Server{
-		// 인수로 받은 net.Listener를 사용하므로, Addr 필드는 지정하지 않음
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		// ListenAndServe 메서드가 아닌 Serve 메서드로 변경
-		if err := s.Serve(l); err != nil &&
-			// http.ErrServerClosed는 http.Server.Shutdown()이 정상적으로 종료되었음을 나타내므로 이상이 아님
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	// 채널로부터의 알림(종료 알림)을 기다림
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-
-	// Go 메서드로 실행한 다른 고루틴의 종료를 기다림.
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
