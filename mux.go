84219cad01a0df8bb566ec726ff54646fd313f5a
HONG-JUNG-WAN <hong43ok@gmail.com>
2024-07-20 18:50:28 +0900

    feat: JSON endpoint, validation, and golden tests
    - Implemented endpoint to receive JSON objects.
    - Added logic to validate JSON objects received in requests.
    - Used defined types to prevent misuse of incorrect values.
    - Implemented several test helpers.
    - Implemented golden tests to store input or expected values in files.

--- mux.go
+++ mux.go
@@ -1,13 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"github.com/Hong/http-handler-modules/handler"
+	"github.com/Hong/http-handler-modules/store"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+	"net/http"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		// 정적 분석 오류를 회피하기 위해 명시적으로 반환값을 버린다.
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+	v := validator.New()
+	mux.Handle("/tasks", &handler.AddTask{Store: store.Tasks, Validator: v})
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
 	return mux
 }
